c8d01ff3ad58c9ee56e0d2cbc7e0df76b9b7fe56
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2021-07-14 14:39:42 +0200

    Add code to setup elastic search docker container

--- service/covid_data_update.sh
+++ service/covid_data_update.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-base_dir="${1:-$HOME}/cord-19-elasticsearch-indexing"
-echo "Working on $base_dir"
+base_dir="${1:-$HOME}"
+our_dir="${base_dir}/cord-19-elasticsearch-indexing"
+# stop elastic search and remove container
 cd $base_dir
+echo "Stopping elastic search container"
+docker-compose stop elastic-search
+yes | docker-compose rm elastic-search
+[[ "$(docker volume ls | grep qaservers_data01 | wc -l)" -gt 0 ]] && yes | docker volume rm qaservers_data01
+docker-compose up -d
+echo "Working on $our_dir"
+cd $our_dir
 source .venv/bin/activate
 python dl_cord19.py \
   --download \
